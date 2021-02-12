82ce42686794f37cec186503be5eb4a9c6770288
Emmanuel Pacaud <emmanuel.pacaud@lapp.in2p3.fr>
2021-02-12 09:56:21 +0100

    Add an executable to project c

--- build.sh
+++ build.sh
@@ -5,11 +5,15 @@ set -e
 script=`readlink -f $0`
 workdir=`dirname ${script}`
 
+rm -rf ${wordir}/install_a
+rm -rf ${wordir}/install_b
+rm -rf ${wordir}/install_c
+
 cd ${workdir}/project_a
 rm -rf build
 meson build --prefix=${workdir}/install_a -Dlibdir=lib
 cd build
-ninja install
+ninja install -v
 
 export PKG_CONFIG_PATH=${workdir}/install_a/lib/pkgconfig
 
@@ -17,7 +21,7 @@ cd ${workdir}/project_b
 rm -rf build
 meson build --prefix=${workdir}/install_b -Dlibdir=lib
 cd build
-ninja install
+ninja install -v
 
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${workdir}/install_b/lib/pkgconfig
 
@@ -25,4 +29,4 @@ cd ${workdir}/project_c
 rm -rf build
 meson build --prefix=${workdir}/install_c -Dlibdir=lib
 cd build
-ninja install
+ninja install -v
---
+++ project_c/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <lib_c.c>
+
+int
+main (int argc, char **argv)
+{
+	printf("Result = %d\n", func_c());
+
+	return EXIT_SUCCESS;
+}
